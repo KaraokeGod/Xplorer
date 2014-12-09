f0abd2002273d832f6e546719b75266e7cfe1376
KaraokeGod <austinly1997@gmail.com>
2014-12-09 09:53:52 -0500

    Started image upload work
    Functionality TBA

--- app/src/main/java/com/hieu/xplorer/MainActivity.java
+++ app/src/main/java/com/hieu/xplorer/MainActivity.java
@@ -1,17 +1,31 @@
 package com.hieu.xplorer;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-
+    private Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button magicButton = (Button)findViewById(R.id.magicbutton);
+        magicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
 
 
