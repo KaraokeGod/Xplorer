2d47a263e9ee46cab4bb9afed20960ddf1ebf8ae
KaraokeGod <austinly1997@gmail.com>
2014-12-18 09:37:09 -0500

    Camera intent and image display working
    Shit’s on fire yo

--- app/src/main/java/com/hieu/xplorer/MainActivity.java
+++ app/src/main/java/com/hieu/xplorer/MainActivity.java
@@ -2,16 +2,27 @@ package com.hieu.xplorer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends Activity {
-    private Uri imageUri;
+    private final static int CAMERA_PIC_REQUEST = 1;
+    private String imagePath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,32 +33,58 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageUri);
-                startActivityForResult(intent, 1);
+
+                // Temporary image file
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                }
+                    catch(Exception e) {
+                    System.out.println("whoops");
+                }
+
+                if (photoFile != null) {
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                }
+                startActivityForResult(intent, CAMERA_PIC_REQUEST);
             }
         });
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // Save a file: path for use with ACTION_VIEW intents
+        imagePath = "file:" + image.getAbsolutePath();
+        return image;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_PIC_REQUEST) {
+            try {
+                // Don't need the file: part
+                Bitmap thumbnail = BitmapFactory.decodeFile(imagePath.substring(5));
+                ImageView image = (ImageView) findViewById(R.id.image);
+                image.setImageBitmap(thumbnail);
+            }
+            catch(Exception e) {
+                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+                System.out.println(e);
+            }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
 }
